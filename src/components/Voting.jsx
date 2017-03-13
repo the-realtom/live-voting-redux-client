e354c76f2807a16a22b40c27fcbccd06fb95e448
the-realtom <thomas.white@connect.qut.edu.au>
2017-03-13 22:16:59 +1000

    initial commit

---
+++ src/components/Voting.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default React.createClass({
+   getPair: function() {
+     return this.prop.pair || [];
+   },
+    render: function () {
+        return <div className="voting">
+            {this.getPair().map(entry =>
+               <button key={entry}>
+                   <h1>{entry}</h1>
+            )}
+        </div>;
+    }
+});
