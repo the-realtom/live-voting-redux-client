a319ffe4fed7bf0268ada57a6261f3a5dc5b3de1
the-realtom <thomas.white@connect.qut.edu.au>
2017-03-19 11:38:36 +1000

    testing component rendering

--- test/components/Voting_spec.js
+++ test/components/Voting_spec.js
@@ -1,5 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+    renderIntoDocument
+} from 'react-addons-test-utils';
 import Voting from '../../src/components/Voting';
 
 describe('Voting', () => {
+
+    it('renders a pair of buttons', () => {
+        const component = renderIntoDocument(
+            <Voting pair={["Trainspotting", "28 Days Later"]} />
+        );
+    });
 
 });
