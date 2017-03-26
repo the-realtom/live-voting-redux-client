66a8cac5568ba58477d61be91192d2a7cb7cfde6
the-realtom <thomas.white@connect.qut.edu.au>
2017-03-26 21:32:40 +1000

    added documentation

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,10 +1,20 @@
 import jsdom from 'jsdom';
 
+/* Create jsdom versions of the document and window objects that would normally be provided by the web
+ browser. Then they need to be put on the global object so that they will be discovered by React
+ when it tries to access document or window.
+ * */
+
 const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
 const win = doc.defaultView;
 
 global.document = doc;
 global.window = win;
+
+/* All the properties that the jsdom window object contains such as navigator, needs to be hoisted
+   onto the Node.js global object. This is done so that properties provided by window can be used
+   without the use of window. prefix which is what would happen in a browser environment.
+* */
 
 Object.keys(window).forEach((key) => {
     if (!(key in global)) {
